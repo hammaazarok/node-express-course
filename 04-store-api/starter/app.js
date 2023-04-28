0c0e1da93eb239f88165240ef2b6e26c764235b9
Hamma Azarok <hammaagazarok@gmail.com>
2023-04-28 16:31:33 +0200

    Answers for lesson 7

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server on ${port}`));
+  } catch (err) {}
+};
+
+start();
