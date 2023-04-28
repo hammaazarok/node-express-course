07ded4e9933c7ca46b895cdba14bd4cac49ad1dd
Hamma Azarok <hammaagazarok@gmail.com>
2023-04-28 11:58:22 +0200

    fix

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+require('dotenv').config();
+const express = require('express');
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    console.log(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server started on ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
