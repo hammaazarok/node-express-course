242c1899d608bbba2adc301490967c9c26af5535
Hamma Azarok <hammaagazarok@gmail.com>
2023-05-10 00:10:12 +0200

    fix

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
   name: {
     type: String,
     required: [true, 'Must provide name'],
